f4c26293c1be85b66a39e61fe5d69a5cd049b114
pedroleandro <pedro.leandrog@gmail.com>
2022-03-19 00:37:29 -0300

    feat(server): implementando server stream

--- services/user.go
+++ services/user.go
@@ -3,12 +3,8 @@ package services
 import (
 	"context"
 	"gRpc-go/pb"
+	"time"
 )
-
-//type UserServiceServer interface {
-//	AddUser(context.Context, *User) (*User, error)
-//	mustEmbedUnimplementedUserServiceServer()
-//}
 
 type UserService struct {
 	pb.UnimplementedUserServiceServer
@@ -24,4 +20,44 @@ func (*UserService) AddUser(context context.Context, request *pb.User) (*pb.User
 		Name:  request.GetName(),
 		Email: request.GetEmail(),
 	}, nil
+}
+
+func (*UserService) AddUserVerbose(request *pb.User, stream pb.UserService_AddUserVerboseServer) error {
+	stream.Send(&pb.UserResultStream{
+		Status: "iniciando",
+		User:   &pb.User{},
+	})
+
+	time.Sleep(time.Second * 2)
+
+	stream.Send(&pb.UserResultStream{
+		Status: "Inserindo",
+		User:   &pb.User{},
+	})
+
+	time.Sleep(time.Second * 3)
+
+	stream.Send(&pb.UserResultStream{
+		Status: "Cadastrado com sucesso",
+		User: &pb.User{
+			Id:    request.GetId(),
+			Name:  request.GetName(),
+			Email: request.GetEmail(),
+		},
+	})
+
+	time.Sleep(time.Second * 5)
+
+	stream.Send(&pb.UserResultStream{
+		Status: "Finalizado",
+		User: &pb.User{
+			Id:    request.GetId(),
+			Name:  request.GetName(),
+			Email: request.GetEmail(),
+		},
+	})
+
+	time.Sleep(time.Second * 2)
+
+	return nil
 }
