2e17c1deb0e1d707ff5909f22ac6dc9b6fc69d98
pedroleandro <pedro.leandrog@gmail.com>
2022-03-19 00:46:23 -0300

    feat(client): implementando client para server stream

--- cmd/client/client.go
+++ cmd/client/client.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"gRpc-go/pb"
 	"google.golang.org/grpc"
+	"io"
 	"log"
 )
 
@@ -19,7 +20,8 @@ func main() {
 
 	client := pb.NewUserServiceClient(connection)
 
-	AddUser(client)
+	AddUserVerbose(client)
+	//AddUser(client)
 }
 
 func AddUser(client pb.UserServiceClient) {
@@ -36,4 +38,31 @@ func AddUser(client pb.UserServiceClient) {
 	}
 
 	fmt.Println(response)
+}
+
+func AddUserVerbose(client pb.UserServiceClient) {
+	request := &pb.User{
+		Id:    "1",
+		Name:  "Pedro Leandro",
+		Email: "pedro.leandrog@gmail.com",
+	}
+
+	responseStream, err := client.AddUserVerbose(context.Background(), request)
+
+	if err != nil {
+		log.Fatalf("Nao foi possivel realizar a requisicao grpc: %v", err)
+	}
+
+	for {
+		stream, err := responseStream.Recv()
+		if err == io.EOF {
+			break
+		}
+
+		if err != nil {
+			log.Fatalf("Nao foi possivel receber os dados a requisicao grpc: %v", err)
+		}
+
+		fmt.Println("Status: ", stream.Status)
+	}
 }
